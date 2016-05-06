6e955112c20d5f927758ea28760b9f4d6ffb888e
Samir Uzunovic <usamir@samirs-mac.local>
2016-05-06 03:59:49 -0700

    Initial Commit

---
+++ Selfies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfies
+//
+//  Created by Samir Uzunovic on 06/05/16.
+//  Copyright © 2016 Samir Uzunovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
